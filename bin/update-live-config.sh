71bcffbac04b8eb0947a23193ad06b18e87510d2
Daniel Baumann <daniel@debian.org>
2010-06-17 17:45:16 +0200

    Automatically determine on which distribution the package is built through lsb_release and removing unecessary config scripts from the resulting package.

---
+++ bin/update-live-config.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+DIRECTORY="${1:-/lib/live/config}"
+
+if [ ! -e "${DIRECTORY}" ]
+then
+	echo "E: ${DIRECTORY} - not found."
+	exit 1
+fi
+
+DISTRIBUTION="$(lsb_release -is | tr [A-Z] [a-z])"
+RELEASE="$(lsb_release -cs | tr [A-Z] [a-z])"
+
+echo "Removing unused scripts..."
+
+case "${DISTRIBUTION}" in
+	debian)
+		# Removing ubuntu scripts
+		rm -f "${DIRECTORY}"/*-apport
+
+		case "${RELEASE}" in
+		lenny)
+			# Removing squeeze and newer scripts
+			rm -f "${DIRECTORY}"/*-gdm3
+			rm -f "${DIRECTORY}"/*-kaboom
+			rm -f "${DIRECTORY}"/*-kde-services
+			rm -f "${DIRECTORY}"/*-keyboard-configuration
+			;;
+
+		*)
+			# Removing lenny legacy scripts
+			rm -f "${DIRECTORY}"/*-console-common
+			rm -f "${DIRECTORY}"/*-console-setup
+			rm -f "${DIRECTORY}"/*-gdm
+			rm -f "${DIRECTORY}"/*-kpersonalizer
+			;;
+		esac
+		;;
+esac
