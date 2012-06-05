4cbdce6dd495d2eb62a1aa0b86092bfe1e3efc4d
Daniel Baumann <daniel@debian.org>
2012-06-05 13:41:37 +0200

    Automatically translate date formats in po files.

--- manpages/bin/update-version.sh
+++ manpages/bin/update-version.sh
@@ -10,9 +10,14 @@
 
 set -e
 
-DATE="$(LC_ALL=C date +%Y\\\\-%m\\\\-%d)"
 PROGRAM="LIVE\\\-CONFIG"
 VERSION="$(cat ../VERSION)"
+
+DATE="$(LC_ALL=C date +%Y\\\\-%m\\\\-%d)"
+
+DAY="$(LC_ALL=C date +%d)"
+MONTH="$(LC_ALL=C date +%m)"
+YEAR="$(LC_ALL=C date +%Y)"
 
 echo "Updating version headers..."
 
@@ -22,3 +27,24 @@ do
 
 	sed -i -e "s|^.TH.*$|.TH ${PROGRAM} ${SECTION} ${DATE} ${VERSION} \"Debian Live Project\"|" ${MANPAGE}
 done
+
+# European date format
+for _LANGUAGE in de es fr it
+do
+	if ls po/${_LANGUAGE}/*.po > /dev/null 2>&1
+	then
+		for _FILE in po/${_LANGUAGE}/*.po
+		do
+			sed -i -e "s|^msgstr .*.2012\"$|msgstr \"${DAY}.${MONTH}.${YEAR}\"|g" "${_FILE}"
+		done
+	fi
+done
+
+# Brazilian date format
+if ls po/pt_BR/*.po > /dev/null 2>&1
+then
+	for _FILE in po/pt_BR/*.po
+	do
+		sed -i -e "s|^msgstr .*-2012\"$|msgstr \"${DAY}-${MONTH}-${YEAR}\"|g" "${_FILE}"
+	done
+fi
