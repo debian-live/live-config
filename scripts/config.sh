6b5fcda47453f150b8720df73fb6bf33f2005ebd
Daniel Baumann <daniel@debian.org>
2012-11-25 09:31:04 +0100

    Assigning internal variables for shortcut options to be reused by local scripts.

--- scripts/config.sh
+++ scripts/config.sh
@@ -73,12 +73,16 @@ Cmdline ()
 				# Disables both console and graphical autologin.
 				_SCRIPTS="${_SCRIPTS:-$(ls /lib/live/config/*)}"
 				LIVE_NOCONFIGS="${LIVE_NOCONFIGS},sysvinit,gdm,gdm3,kdm,lightdm,lxdm,nodm,slim,upstart,xinit"
+
+				_NOAUTOLOGIN="true"
 				;;
 
 			live-config.nottyautologin|nottyautologin)
 				# Disables console autologin.
 				_SCRIPTS="${_SCRIPTS:-$(ls /lib/live/config/*)}"
 				LIVE_NOCONFIGS="${LIVE_NOCONFIGS},sysvinit,upstart"
+
+				_NOTTYAUTOLOGIN="true"
 				;;
 
 			live-config.nox11autologin|nox11autologin)
@@ -86,6 +90,8 @@ Cmdline ()
 				# mechanism
 				_SCRIPTS="${_SCRIPTS:-$(ls /lib/live/config/*)}"
 				LIVE_NOCONFIGS="${LIVE_NOCONFIGS},gdm,gdm3,kdm,lightdm,lxdm,nodm,slim,xinit"
+
+				_NOX11AUTOLOGIN="true"
 				;;
 
 			# Special options
